31cfbf5d78b872061843831e415f4772517f363c
brendan.stein <brendan.stein@payat.co.za>
2024-07-26 14:39:51 +0200

    Section 3 Lesson 31

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,34 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+    @Autowired //tells spring context to inject a context
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test //using the application context to wired in the bean
+    void testGetControllerFromCtx(){
+        MyController myController = applicationContext.getBean(MyController.class);
+        System.out.println(myController.sayHello());
+    }
+
+    @Test //using autowired to wired in the bean
+    void testAutowiredOfController(){
+        System.out.println(myController.sayHello());
+    }
+
     @Test
     void contextLoads() {
+
     }
 
 }
