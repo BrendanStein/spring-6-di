29db86050846513d261eb568c4931c36f5858b5c
brendan.stein <brendan.stein@payat.co.za>
2024-07-30 17:13:34 +0200

    Section 3 Lesson 35

--- src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring6di.services;
 
+import org.springframework.stereotype.Service;
+
+@Service //indicates a spring component
 public class GreetingServiceImpl implements GreetingService {
 
     @Override
     public String sayGreeting() {
+
         return "Hello Everyone from Base Service";
     }
 }
