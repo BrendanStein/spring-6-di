abeb9052a02ba74de29966f9c4cff2e0539ad933
brendan.stein <brendan.stein@payat.co.za>
2024-07-31 09:44:46 +0200

    Section 3 Lesson 36

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,11 +1,14 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller //spring managed component
 public class ConstructorInjectedController
 {
     private final GreetingService greetingService;
 
+    // spring autowires constructor injection
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/guru/springframework/spring6di/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController
 {
+    @Autowired //manually autowire when no constructor injection, error message says this method is not recommended
     GreetingService greetingService;
 
     public String sayHello(){
